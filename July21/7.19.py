d3b10e13cd94acece71f21d09bce5eafa7ad2270
Jordan McGhee <jordanmcghee95@gmail.com>
2021-07-19 22:42:27 -0400

    added sum of minimums problem

--- July21/7.19.py
+++ July21/7.19.py
@@ -60,3 +60,37 @@ over_the_road(3, 5) = 8
 def over_the_road(address, n):
     # Since there are an identical number of houses on both sides of the road, we can multiply n by 2 to get what would be the number of the first house on the opposite side of the road. Now, to figure out the number of the house across from the given address, we need to subtract the given address and subtract 1 as well. I'm honestly a little confused on why this works, so hopefully nobody sees this note. Solved it in one line though
     return n*2-(address-1)
+
+"""
+
+SUM OF MINIMUMS
+
+Given a 2D list of size m * n. Your task is to find the sum of minimum value in each row.
+
+For Example:
+
+[
+    [1, 2, 3, 4, 5],       # minimum value of row is 1
+    [5, 6, 7, 8, 9],       # minimum value of row is 5
+    [20, 21, 34, 56, 100]  # minimum value of row is 20
+]
+So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+
+Note: You will be always given non-empty list containing Positive values.
+
+"""
+
+def sum_of_minimums(numbers):
+    # create empty list to store minimum values for each list in numbers
+    mins = []
+
+    # iterate over every list in numbers
+    for lst in numbers:
+        # append the minimum from each list to our variable list mins
+        mins.append(min(lst))
+    
+    # return the sume of the values in mins
+    return sum(mins)
+
+# print(sum_of_minimums([ [ 7,9,8,6,2 ], [6,3,5,4,3], [5,8,7,4,5] ])) # should be 9
+# print(sum_of_minimums([ [11,12,14,54], [67,89,90,56], [7,9,4,3], [9,8,6,7]])) # should be 76
